d64477796c66e0e11f4d1cdd45b6ec258b725efe
baicheng <bcfang18@gmail.com>
2025-12-15 18:05:13 -0500

    Add hw1 and hw2

---
+++ Coding/Baicheng_Fang/hw2/p5_main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        Employee e2 = new Employee("Foo", 007);
+        Employee e3 = new Employee("Bar", 008, 100000)
+    }
+}
