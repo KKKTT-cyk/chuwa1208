42c31d16f42b91a18ed1fc0dd7637ca0de6dfc41
AkikozzzzzzM <113269465+AkikozZM@users.noreply.github.com>
2025-12-19 12:38:26 -0800

    hw4

---
+++ Coding/MultiLevelExceptionSystem/OrderException.java
@@ -0,0 +1,14 @@
+package Coding.MultiLevelExceptionSystem;
+
+public class OrderException extends RuntimeException {
+    private final IErrorCode errorCode;
+
+    public OrderException(IErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public IErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
---
+++ Coding/ResourceManagement/LogWriter.java
@@ -0,0 +1,19 @@
+package Coding.ResourceManagement;
+
+public class LogWriter implements AutoCloseable {
+    private String filename;
+
+    public LogWriter(String filename) {
+        this.filename = filename;
+        System.out.println("Opening log file: " + filename);
+    }
+
+    public void writeLog(String message) {
+        System.out.println("Writing to " + filename + ": " + message);
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Closing log file: " + filename);
+    }
+}
